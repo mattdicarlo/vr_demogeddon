05f970b7cb6e2cbcd6e0ac57c28819ae0054ebf6
awu <wufire@gmail.com>
2016-07-17 00:36:39 -0500

    Throw-capable hands with Animation * Mostly ported over and cleaned up throw code from another project * Demo Scene should have an incredibly basic SteamVR setup with tracked controller hands that can throw and animate * Prefabs for the headset and hands that I've found easy to use. * haven't tested this particular setup in VR, yet...

---
+++ Assets/Throw.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class Throw : MonoBehaviour
+{
+    public GameObject selected;
+
+    public Transform attachPoint;
+
+    SteamVR_TrackedObject trackedObj;
+    FixedJoint joint;
+    Rigidbody throwRigidbody;
+
+    public Collider handCollider;
+    public GameObject handModel;
+
+    Animator handAnimator;
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        throwRigidbody = GetComponent<Rigidbody>();
+        handAnimator = handModel.GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        var device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        // Find and highlight a selected object
+        selected = SelectNearbyObject();
+
+        if (joint == null && device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            handAnimator.SetBool("ShouldGrip", true);
+            if (selected != null)
+            {
+                //handCollider.enabled = false;
+                selected.transform.position = attachPoint.position;
+                joint = selected.AddComponent<FixedJoint>();
+                joint.connectedBody = throwRigidbody;
+            }
+        }
+        else if (joint != null && device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            handAnimator.SetBool("ShouldGrip", false);
+            StartCoroutine(ThrowObject(device));
+        }
+    }
+
+    private IEnumerator ThrowObject(SteamVR_Controller.Device device)
+    {
+        var go = joint.gameObject;
+        var rigidbody = go.GetComponent<Rigidbody>();
+        Object.Destroy(joint);
+        joint = null;
+
+        yield return null;
+
+        // We should probably apply the offset between trackedObj.transform.position
+        // and device.transform.pos to insert into the physics sim at the correct
+        // location, however, we would then want to predict ahead the visual representation
+        // by the same amount we are predicting our render poses.
+
+        var origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+        if (origin != null)
+        {
+            rigidbody.velocity = origin.TransformVector(device.velocity);
+            rigidbody.angularVelocity = origin.TransformVector(device.angularVelocity);
+        }
+        else
+        {
+            rigidbody.velocity = device.velocity;
+            rigidbody.angularVelocity = device.angularVelocity;
+        }
+
+        rigidbody.maxAngularVelocity = rigidbody.angularVelocity.magnitude;
+
+        yield return null;
+        yield return null;
+        yield return null;
+        //handCollider.enabled = true;
+    }
+
+    private GameObject SelectNearbyObject()
+    {
+        Collider[] nearHandObjects = Physics.OverlapSphere(transform.position, 0.1f);
+        foreach (Collider col in nearHandObjects)
+        {
+            if (col.GetComponent<Rigidbody>() != null && col.gameObject.tag != "nonpickup")
+            {
+                return col.gameObject;
+            }
+        }
+        return null;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("Colliding with: " + collision.gameObject.name);
+    }
+}
