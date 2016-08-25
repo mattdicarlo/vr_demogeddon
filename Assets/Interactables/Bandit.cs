15ebc6e0afc2cc08a82eaef78cda4e339057d04d
Matt DiCarlo <dicarlo.matt@gmail.com>
2016-08-24 20:16:13 -0500

    Added basic handling of lever pull and win/lose display.

---
+++ Assets/Interactables/Bandit.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bandit : MonoBehaviour {
+
+    private Lever _lever;
+    private BanditScreen _screen;
+    private bool _isShowingResults;
+
+	// Use this for initialization
+	void Start () {
+        _isShowingResults = false;
+        _lever = GetComponentInChildren<Lever>();
+        _screen = GetComponentInChildren<BanditScreen>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (_lever.Value >= 1 && !_isShowingResults) {
+            StartCoroutine(ResultsDisplay());
+        }
+	}
+
+    IEnumerator ResultsDisplay() {
+        if (_isShowingResults) {
+            yield break;
+        }
+
+        _isShowingResults = true;
+        if (Random.Range(0, 100) > 90) { 
+            _screen.setToWin();
+        }
+        else {
+            _screen.setToLose();
+        }
+
+        yield return new WaitForSeconds(5);
+        _screen.setToBase();
+        _isShowingResults = false;
+    }
+}
---
+++ Assets/Interactables/BanditScreen.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BanditScreen : MonoBehaviour {
+    public Material _baseImage;
+    public Material _winImage;
+    public Material _loseImage;
+
+	// Use this for initialization
+	void Start () {
+        setToBase();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setToBase() {
+        GetComponent<Renderer>().material = _baseImage;
+    }
+
+    public void setToWin()
+    {
+        GetComponent<Renderer>().material = _winImage;
+    }
+
+    public void setToLose()
+    {
+        GetComponent<Renderer>().material = _loseImage;
+    }
+}
